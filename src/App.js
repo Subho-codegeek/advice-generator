beea03c7086162e86626bc42ac439003d1b99d65
Subhamay Paul <subhomaypaul@gmail.com>
2021-10-20 23:32:05 +0530

    deploying my advice generator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [advice, setadvice] = useState('');
+
+  useEffect(() => {
+    fetchAdvice();
+  });
+
+  const fetchAdvice = async ()=>{
+    const responce = await axios.get('https://api.adviceslip.com/advice');
+    setadvice(responce.data.slip.advice);
+    // console.log(advice);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <h2 className='heading'>Have some advices for free</h2> 
+      </div>
+      <div className='card'>
+        <h1>{advice}</h1>
+        <button className='button' onClick={fetchAdvice}>
+          <span>
+            GIVE ME ADVICE!
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
